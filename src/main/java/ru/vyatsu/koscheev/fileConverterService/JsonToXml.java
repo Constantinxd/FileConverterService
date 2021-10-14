d882565e5c686038c0e52c763fcee0158e51f54e
Konstantin <stud115857@vyatsu.ru>
2021-10-14 16:57:16 +0300

    json

--- src/main/java/ru/vyatsu/koscheev/fileConverterService/JsonToXml.java
+++ src/main/java/ru/vyatsu/koscheev/fileConverterService/JsonToXml.java
@@ -1,5 +1,9 @@
 package ru.vyatsu.koscheev.fileConverterService;
 
+import ru.vyatsu.koscheev.fileConverterService.XmlToJson.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class JsonToXml {
     public static void main(String[] args) {
 
